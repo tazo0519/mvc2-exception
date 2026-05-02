3206b10b55ed6e4abd23d99759b67c752ecc9710
tazo0519 <tazo0519@naver.com>
2026-05-02 15:19:36 +0900

    Project Initialization

---
+++ src/test/java/tazo0519/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package tazo0519.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
